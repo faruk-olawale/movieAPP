72575d30742814e1631399a2a654e1b0f110ed51
faruk <farukolawale509@gmail.com>
2025-11-03 17:09:40 +0100

    initial message

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -3,7 +3,7 @@ import "../css/Navbar.css"
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">CineFaruk</Link>
+            <Link to="/">FarukCinema</Link>
         </div>
         <div className="navbar-Links">
             <Link to="/" className="nav-link">Home</Link>
